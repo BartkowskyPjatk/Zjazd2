d7a7576fd160bf56c8800db6ad7495fcd4ffb398
Bartek <bartek.bicki@gmail.com>
2024-04-21 00:00:57 +0200

    prawie koniec

--- Cwiczenie/src/Main.java
+++ Cwiczenie/src/Main.java
@@ -1,7 +1,44 @@
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        //tworzę studenta s31215
+        ArrayList<Integer> gradesS1215= new ArrayList<>();
+        gradesS1215.add(3);
+        gradesS1215.add(2);
+        gradesS1215.add(3);
+        Student s31215 = new Student("Jan", "Kowalski", 31215, "s31215", "Szkolna 13",gradesS1215);
+
+        //tworzę studenta s99999
+        ArrayList<Integer> gradesS99999= new ArrayList<>();
+        gradesS1215.add(5);
+        gradesS1215.add(5);
+        gradesS1215.add(5);
+        Student s99999 = new Student("Franek", "Michalski", 99999, "s99999", "Francuska 20",gradesS99999);
+
+
+
+        //tworzę grupe
+        ArrayList<Student> gr12StudentList = new ArrayList<>();
+        gr12StudentList.add(s31215);
+        StudentGroup gr12 = new StudentGroup("gr12", gr12StudentList);
+
+
+        gr12.addingStudentToGroup(s99999 ,gr12);
+        gr12.addingStudentToGroup(s31215 ,gr12);
+
+        gr12.soutStudentList(gr12);
+
+        gr12.removingStudentFromGroup(s99999 , gr12);
+
+        gr12.soutStudentList(gr12);
+
     }
+
 
 }
--- Cwiczenie/src/StudentGroup.java
+++ Cwiczenie/src/StudentGroup.java
@@ -2,6 +2,33 @@ import java.util.ArrayList;
 
 public class StudentGroup {
     public String name;
+    public ArrayList<Student> studentList = new ArrayList<Student>();
 
-    public ArrayList<Student> StudentList= new ArrayList<Student>();
+    public StudentGroup(String name, ArrayList<Student> studentList){
+        this.name = name;
+        this.studentList = studentList;
+    }
+
+    public void addingStudentToGroup( Student pajac, StudentGroup Group){
+        if (Group.studentList.size() >= 15) throw new IllegalArgumentException("grupa ma już 15 czlonkow");
+        Group.studentList.add(pajac);
+        System.out.println("Dodano studenta " + pajac.fname +" "+ pajac.lname +" s"+pajac.indexNumber + " do grupy "+ Group.name );
+    }
+    public void removingStudentFromGroup( Student pajac, StudentGroup Group){
+        if (Group.studentList.size() <= 0) throw new IllegalArgumentException("grupa ma 0 czlonkow");
+        Group.studentList.remove(pajac);
+        System.out.println("Usunieto studenta " + pajac.fname +" "+ pajac.lname +" s"+pajac.indexNumber + " z grupy "+ Group.name );
+        System.out.println();
+    }
+    public void soutStudentList(StudentGroup Group){
+        for(Student studencik : Group.studentList){
+            System.out.println("Imię : "+studencik.fname + " Nazwisko: "/* + studencik.lname + " NumerIndexu: " + studencik.indexNumber + " e-mail: " + studencik.email
+                    + " adres : " + studencik.address + " oceny: "*/); soutStudentGrades(studencik.grades);
+        }
+    }
+    public void soutStudentGrades(ArrayList<Integer> grades){
+        for (int grade : grades) {
+            System.out.print(grade);
+        }
+    }
 }
